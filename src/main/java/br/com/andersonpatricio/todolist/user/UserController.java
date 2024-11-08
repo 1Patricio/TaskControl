a262ae73e10a5ce6c50aa2a0d29aa7b1098d2e25
1Patricio <andersongpatricio@hotmail.com>
2024-11-07 22:39:07 -0300

    HTTP Response e Criptografia de Senha

--- src/main/java/br/com/andersonpatricio/todolist/user/UserController.java
+++ src/main/java/br/com/andersonpatricio/todolist/user/UserController.java
@@ -1,10 +1,14 @@
 package br.com.andersonpatricio.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping ("/users")
@@ -14,14 +18,24 @@ public class UserController { //classe
     private IUserRepository userRepository;
 
     @PostMapping("/") //acessar esse controller
-    public UserModel create(@RequestBody UserModel userModel){   // Método
+    public ResponseEntity create(@RequestBody UserModel userModel){   // Método
         // System.out.println(userModel.getUsername());
         var user = this.userRepository.findByUsername(userModel.getUsername());
+
         if (user != null) {                                      // Se o usuário já existir
             System.out.println("Usuário já existe");
-            return null;
+            // Vamos Retornar
+            // Mensagem de Erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");    // Caso de erro 400, exiba
         }
+
+        // Hash de senha  // Passa um array por conta que pede no método
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray()); 
+        
+        userModel.setPassword(passwordHashred); // Set senha criptografada 
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
