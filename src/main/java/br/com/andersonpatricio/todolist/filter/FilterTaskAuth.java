e2ac35ad4596053dc95bfb01ed718d1d4c84a0bb
1Patricio <andersongpatricio@hotmail.com>
2024-12-02 16:01:47 -0300

    Continuação Filtro

--- src/main/java/br/com/andersonpatricio/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/andersonpatricio/todolist/filter/FilterTaskAuth.java
@@ -1,27 +1,49 @@
 package br.com.andersonpatricio.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.Filter;   //servlet é a base para frameworks web do java
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                // Pegar a autenticação (usuário e senha)
+                var authorization = request.getHeader("Authorization");
 
-        //Executar alguma ação
-        // Podemos barrar ou continuar
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);   // Queremos que ele continue
+                var authEncoded = authorization.substring("Basic".length()).trim();   // Pegar a palavrar Basic e calcular tamanho dela "5". Remova os espaços
+                
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);          // Devolve ainda criptografado
+
+                var authString = new String(authDecode);                              // Devolver USUARIO:SENHA
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+
+                System.out.println("Usuário :" + username);
+                System.out.println("Senha :" + password);
+
+
+                //  * Validar Usuário
+                //  * Validar senha
+                //  * Segue
+
+                filterChain.doFilter(request, response);
     }
+
 
     
 }
