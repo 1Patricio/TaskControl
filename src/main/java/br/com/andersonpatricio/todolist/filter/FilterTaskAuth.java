9412a2336838e57ab0809daf80f216d175d95bf4
1Patricio <andersongpatricio@hotmail.com>
2024-12-06 14:02:22 -0300

    Validando rota e decode de senha

--- src/main/java/br/com/andersonpatricio/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/andersonpatricio/todolist/filter/FilterTaskAuth.java
@@ -3,10 +3,13 @@ package br.com.andersonpatricio.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;   //servlet é a base para frameworks web do java
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.andersonpatricio.todolist.user.IUserRepository;
+import jakarta.servlet.Filter; //servlet é a base para frameworks web do java
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -17,33 +20,46 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-                // Pegar a autenticação (usuário e senha)
-                var authorization = request.getHeader("Authorization");
 
-                var authEncoded = authorization.substring("Basic".length()).trim();   // Pegar a palavrar Basic e calcular tamanho dela "5". Remova os espaços
-                
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);          // Devolve ainda criptografado
+        var servletPath = request.getServletPath();
 
-                var authString = new String(authDecode);                              // Devolver USUARIO:SENHA
+        if (servletPath.equals("/tasks/")) {
+            // Pegar a autenticação (usuário e senha)
+            var authorization = request.getHeader("Authorization");
 
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+            var authEncoded = authorization.substring("Basic".length()).trim(); // Pegar a palavrar Basic e calcular
+                                                                                // tamanho
+                                                                                // dela "5". Remova os espaços
 
-                System.out.println("Usuário :" + username);
-                System.out.println("Senha :" + password);
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded); // Devolve ainda criptografado
 
+            var authString = new String(authDecode); // Devolver USUARIO:SENHA
 
-                //  * Validar Usuário
-                //  * Validar senha
-                //  * Segue
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
 
-                filterChain.doFilter(request, response);
+            // * Validar Usuário
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // * Validar senha
+                var passowrdVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passowrdVerify.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
-
-
-    
 }
